99a191c3d99d181a31e160ef2079d84493e48fd4
brandon <brandonescobedo1234@gmail.com>
2024-01-26 23:27:22 -0600

    Se empezo mis cuestionarios

--- Anksus_WebAPI/obj/Debug/net8.0/Anksus_WebAPI.Server.AssemblyInfo.cs
+++ Anksus_WebAPI/obj/Debug/net8.0/Anksus_WebAPI.Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Anksus_WebAPI.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b81c64947164f3ec99019c31902a8ac52587250")]
 [assembly: System.Reflection.AssemblyProductAttribute("Anksus_WebAPI.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Anksus_WebAPI.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
